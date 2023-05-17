3179a811a7287ae9ee10be9f40951e02bad540d1
Daiana <dauipoma@gmail.com>
2023-05-17 18:23:28 +0200

    Add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { Login } from './pages/Login'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { AuthContextProvider } from './context/AuthContext'
+import { FilesContextProvider } from './context/FilesContext'
+
 export default function App() {
-  return <h1>Drop Zone</h1>
+  return (
+    <main className='text-center min-h-screen flex flex-wrap-reverse gap-x-16 p-4 items-center justify-center'>
+      <FilesContextProvider>
+        <AuthContextProvider>
+          <Routes>
+            <Route element={<ProtectedRoute />}>
+              <Route path='/' element={<Home />} />
+            </Route>
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </AuthContextProvider>
+      </FilesContextProvider>
+    </main>
+  )
 }
